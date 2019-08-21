39d438dfc9e87afb653d0b3e75f9acc1d4e40f67
frantisek.konecny <fr.konecny@gmail.com>
2019-08-21 09:29:27 +0200

    list of products working

--- src/main/java/com/uhk/fim/ppro/services/ProductServiceImpl.java
+++ src/main/java/com/uhk/fim/ppro/services/ProductServiceImpl.java
@@ -34,34 +34,34 @@ public class ProductServiceImpl implements ProductService{
         products.put(1, product1);
 
         Product product2 = new Product();
-        product1.setId(2);
-        product1.setDescription("Product 2");
-        product1.setPrice(new BigDecimal("32.99"));
-        product1.setImageUrl("http://example.com/product2");
+        product2.setId(2);
+        product2.setDescription("Product 2");
+        product2.setPrice(new BigDecimal("32.99"));
+        product2.setImageUrl("http://example.com/product2");
 
         products.put(2, product2);
 
         Product product3 = new Product();
-        product1.setId(3);
-        product1.setDescription("Product 3");
-        product1.setPrice(new BigDecimal("15.99"));
-        product1.setImageUrl("http://example.com/product3");
+        product3.setId(3);
+        product3.setDescription("Product 3");
+        product3.setPrice(new BigDecimal("15.99"));
+        product3.setImageUrl("http://example.com/product3");
 
         products.put(3, product3);
 
         Product product4 = new Product();
-        product1.setId(4);
-        product1.setDescription("Product 4");
-        product1.setPrice(new BigDecimal("44.99"));
-        product1.setImageUrl("http://example.com/product4");
+        product4.setId(4);
+        product4.setDescription("Product 4");
+        product4.setPrice(new BigDecimal("44.99"));
+        product4.setImageUrl("http://example.com/product4");
 
         products.put(4, product4);
 
         Product product5 = new Product();
-        product1.setId(5);
-        product1.setDescription("Product 5");
-        product1.setPrice(new BigDecimal("882.99"));
-        product1.setImageUrl("http://example.com/product5");
+        product5.setId(5);
+        product5.setDescription("Product 5");
+        product5.setPrice(new BigDecimal("882.99"));
+        product5.setImageUrl("http://example.com/product5");
 
         products.put(5, product5);
 
